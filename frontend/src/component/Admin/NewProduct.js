e20e28a08f2f66ed0303e71f08fed31a778909ed
Piyush2504 <81811435+Piyush2504@users.noreply.github.com>
2023-04-14 10:31:02 +0530

    changes in sidebar and product

--- frontend/src/component/Admin/NewProduct.js
+++ frontend/src/component/Admin/NewProduct.js
@@ -28,13 +28,13 @@ const NewProduct = ({ history }) => {
   const [imagesPreview, setImagesPreview] = useState([]);
 
   const categories = [
-    "Laptop",
-    "Footwear",
-    "Bottom",
-    "Tops",
-    "Attire",
-    "Camera",
-    "SmartPhones",
+    "Apple",
+    "Avocado",
+    "Mango",
+    "Orange",
+    "Pineapple",
+    "Watermelon",
+    "Banana",
   ];
 
   useEffect(() => {
